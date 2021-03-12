aed106548cd16ad72b0d81062075a8ad6bc39a08
GoudekettingRM <r.goudeketting@gmail.com>
2021-03-12 23:50:53 +0100

    Add chapter loading and loader

---
+++ ChapterLoad.js
@@ -0,0 +1,76 @@
+const ChapterDownloadConfig = {
+  formElementSelector: '[data-download-chapter-form]',
+  chapterContainer: '[data-chapter-container]',
+  chapterNumberContainer: '[data-chapter-number]',
+  loadingPlaceholder: '[data-loading-placeholder]',
+};
+
+class ChapterDownload {
+  static initialize() {
+    const form = document.querySelector(
+      ChapterDownloadConfig.formElementSelector,
+    );
+
+    const initializedChapterDownload = new ChapterDownload(form);
+    initializedChapterDownload.initializeBehavior();
+  }
+
+  constructor(form) {
+    this.form = form;
+    this.inputField = form.querySelector('input[type=number]');
+    this.maxChapterNumber = Math.pow(10, 100) / 5000; // One googol / 5000 words per chapter
+
+    this.chapterContainer = document.querySelector(
+      ChapterDownloadConfig.chapterContainer,
+    );
+
+    this.chapterNumberContainer = this.chapterContainer.querySelector(
+      ChapterDownloadConfig.chapterNumberContainer,
+    );
+
+    this.loadingPlaceholder = document.querySelector(
+      ChapterDownloadConfig.loadingPlaceholder,
+    );
+
+    for (let i = 0; i < 5000; i++) {
+      const span = document.createElement('span');
+      span.innerHTML += 'Robin ';
+      this.chapterContainer.appendChild(span);
+    }
+  }
+
+  initializeBehavior() {
+    this.form.addEventListener('submit', (e) => this.getChapter(e));
+  }
+
+  getChapter(event) {
+    event.preventDefault();
+
+    if (!this.inputField.value) {
+      alert('You have to enter your favorite chapter, silly!');
+      return null;
+    }
+
+    const chapterNumber = parseInt(this.inputField.value);
+
+    if (chapterNumber > this.maxChapterNumber) {
+      alert(
+        "Don't be silly! That chapter number doesn't exist of course. That's way to large!",
+      );
+      return null;
+    }
+
+    this.chapterContainer.classList.add('hidden');
+    this.loadingPlaceholder.classList.remove('hidden');
+
+    setTimeout(() => {
+      this.loadingPlaceholder.classList.add('hidden');
+      this.chapterNumberContainer.textContent = chapterNumber;
+      this.chapterContainer.classList.remove('hidden');
+    }, 1500);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ChapterDownload.initialize();
+});
--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-const main = () => {
-  const googol = Math.pow(10, 100);
-  let newLoads = 0.5;
-
-  const main = document.querySelector('main');
-
-  loadTenThousandRobins(main);
-
-  document.addEventListener(
-    'scroll',
-    debounce(() => {
-      if (
-        main.getBoundingClientRect().bottom <= 2 * window.innerHeight &&
-        newLoads < googol
-      ) {
-        loadTenThousandRobins(main);
-        newLoads += 0.5;
-      } else if (newLoads === googol) {
-        const end = document.createElement('h2');
-        end.innerHTML = 'You passed a googol of Robins!';
-        main.appendChild(end);
-        newLoads += 0.5;
-      }
-    }),
-    25,
-  );
+const MainConfig = {
+  mainElementSelector: '[data-Googol-container]',
 };
 
-const loadTenThousandRobins = (main) => {
-  for (let i = 0; i < 5000; i++) {
-    const span = document.createElement('span');
-    span.innerHTML += 'Robin ';
-    main.appendChild(span);
+class Main {
+  static initialize() {
+    const main = document.querySelector(MainConfig.mainElementSelector);
+
+    const initializedMain = new Main(main);
+    initializedMain.initializeBehavior();
   }
-};
+
+  constructor(main) {
+    this.main = main;
+    this.Googol = Math.pow(10, 100);
+    this.perLoad = 5000;
+    this.requiredLoads = this.Googol / this.perLoad;
+
+    this.performedLoadsCount = 0;
+
+    this.loadMoreRobins();
+  }
+
+  initializeBehavior() {
+    document.addEventListener(
+      'scroll',
+      debounce(() => this.nextAction(), 50),
+    );
+  }
+
+  nextAction() {
+    if (
+      this.main.getBoundingClientRect().bottom <= 10 * window.innerHeight &&
+      this.performedLoadsCount < this.requiredLoads
+    ) {
+      this.loadMoreRobins();
+    } else if (this.performedLoadsCount === this.requiredLoads) {
+      const end = document.createElement('h2');
+      end.innerHTML = 'You passed a Googol of Robins!';
+      this.main.appendChild(end);
+      this.incrementPerformedLoadCount();
+    }
+  }
+
+  loadMoreRobins() {
+    for (let i = 0; i < this.perLoad; i++) {
+      const span = document.createElement('span');
+      span.innerHTML += 'Robin ';
+      this.main.appendChild(span);
+    }
+    this.incrementPerformedLoadCount();
+  }
+
+  incrementPerformedLoadCount() {
+    this.performedLoadsCount += 1;
+  }
+}
 
 function debounce(callback, wait, immediate) {
   let timeout;
@@ -50,4 +72,6 @@ function debounce(callback, wait, immediate) {
   };
 }
 
-document.addEventListener('DOMContentLoaded', () => main());
+document.addEventListener('DOMContentLoaded', () => {
+  Main.initialize();
+});
